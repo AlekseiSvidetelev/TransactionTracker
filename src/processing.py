7a202edcd1487bd5a549e40f20810b3bbbabe87c
Aleksei <svid.aleksei@ya.ru>
2025-01-01 17:02:16 +0300

    Update processing.py

--- src/processing.py
+++ src/processing.py
@@ -15,7 +15,7 @@ def filter_by_state(
 
 
 def sort_by_date(
-    sort_list: list[dict[str, Union[str, int]]], revers_sort: bool = True
+    sort_list: list[dict[str, Union[str, int]]], revers_sort: bool = False
 ) -> list[dict[str, Union[str, int]]]:
     """Функция для сортировки списка словарей по дате"""
     sorted_list = sorted(sort_list, key=lambda x: format_date(x["date"]), reverse=revers_sort)
@@ -43,5 +43,15 @@ if __name__ == "__main__":
                 {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
             ],
             True,
+        )
+    )
+    print(
+        sort_by_date(
+            [
+                {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+                {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+                {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+                {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+            ]
         )
     )
